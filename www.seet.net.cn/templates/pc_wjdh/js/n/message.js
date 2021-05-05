df309e9c1433a4f430810c70da0e5bbcc10fe542
jay bhinsara <jaybhinsara9@gmail.com>
2021-05-05 16:24:39 +0530

    seet

--- www.seet.net.cn/templates/pc_wjdh/js/n/message.js
+++ www.seet.net.cn/templates/pc_wjdh/js/n/message.js
@@ -14,49 +14,49 @@ $(function () {
         var kcode = $('#msgcode').val().trim();
         var reg = /(^(0[0-9]{2,3}\-)?([2-9][0-9]{6,7})+(\-[0-9]{1,4})?$)|(^((\(\d{3}\))|(\d{3}\-))?(1[3578]\d{9})$)|(^400[0-9]{7})/;
         if(kuser == "" && kphone == "" && kcontent ==""){
-        	alert("此表单不能为空提交,请您完善相关信息！");
+        	alert("This form cannot be blank to submit, please complete the relevant information!");
         	return false;
         }
 
         if (kuser == "") {
-            alert('姓名不能为空！')
+            alert('The name cannot be empty!')
             $("#msgName").focus();
             return false;
         }
         if (kphone == "") {
-            alert('号码不能为空！')
+            alert('The number cannot be empty!')
             $("#msgTel").focus();
             return false;
         }
         if(!reg.test(kphone)) {
-            alert('号码填写有误,请您输入正确的号码!')
+            alert('The number is incorrect, please enter the correct number!')
             $("#msgTel").focus();
             return false;
         }
         if (kcontent == "" || kcontent.length < 10) {
-            alert('留言内容必须大于10字！')
+            alert('The message content must be greater than 10 words!')
             $("#msgContext").focus();
             return false;
         }
         if (kcode == "") {
-            alert('验证码不能为空')
+            alert('verification code must be filled')
             $("#msgcode").focus();
             return false;
         }
         $.post('/api/message.ashx?action=add', {
-            "kcontent": '信息：' + kcontent ,
+            "kcontent": 'information:' + kcontent ,
             "kuser": kuser,
             "kphone": kphone, 
             "kcode": kcode
         }, function (res) {
             if (res == '1') {
-                alert("留言提交成功");
+                alert("Message submitted successfully");
                 $('#msgContext').val('');
                 $('#msgName').val('');
                 $('#msgTel').val('');
                 $('#msgcode').val('');
             }else {
-                alert('留言提交失败')
+                alert('Message submission failed')
             }
         }) 
     })
